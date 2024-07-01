7be1b5e54ff21e63a647719497986fc666976378
mahuena <ads23a00024y@ait.edu.gh>
2024-07-01 14:51:43 +0000

    creating routes for the three pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { LoginPage } from './Login/LoginPage.jsx'
 import {Home} from './home/Home.jsx'
 import {RegistrationPage} from './Registration/RegistrationPage.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import {TumorPage} from "./tumor/TumorPage.jsx";
+import {HeartAttackPage} from "./heartAttack/HeartAttackPage.jsx";
+
 function App() {
 
   return (
@@ -12,6 +15,9 @@ function App() {
                     <Route path="/" element={<LoginPage />} />
                     <Route path="/registrationPage" element={<RegistrationPage />} />
                     <Route path="/home" element={<Home />} />
+                    <Route path='/tumorPage' element={<TumorPage />}/>
+                    <Route path='/pneumoniaPage' element={<TumorPage />}/>
+                    <Route path='/heartAttackPage' element={<HeartAttackPage />}/>
                 </Routes>
             </BrowserRouter>
       </ChakraProvider>
