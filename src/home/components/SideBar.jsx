7f8ba0ae0b21167cce8a0c3a413afd7efe06c1d7
mahuena <ads23a00024y@ait.edu.gh>
2024-07-01 13:48:08 +0000

    done with the sidebar component

--- src/home/components/SideBar.jsx
+++ src/home/components/SideBar.jsx
@@ -1,10 +1,28 @@
+import {Drawer, DrawerContent, DrawerOverlay, IconButton, useDisclosure} from "@chakra-ui/react";
+import {HamburgerIcon} from "@chakra-ui/icons";
+
 export const SideBar = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
         <>
-            <div className="bg-blue w64">
-                <h1 className="text-3xl font-bold underline">Logo</h1>
-
+            <div className="bg-[#1e1e1e] w-80 h-screen hidden lg:block">
+                <div className={"sidebarContent"}>
+                    <div className="border-b border-gray-700 border-dashed p-8 flex justify-between">
+                        <h1 className="font-bold text-white text-4xl ">Logo</h1>
+                    </div>
+                </div>
             </div>
+
+            <IconButton aria-label={'see menu'} icon={<HamburgerIcon />} onClick={onOpen} ml={3} mt={6} display={{ base: "block", lg: "none" }}/>
+            <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay />
+                <DrawerContent bg="#1e1e1e">
+                    <div className="border-b border-gray-700 border-dashed p-8 ml-3">
+                        <h1 className="font-bold text-white text-4xl ">Logo</h1>
+                    </div>
+                </DrawerContent>
+            </Drawer>
         </>
     )
 }
