c570cb4b17f1ae5fff24b7c7964958773a4e43d9
mahuena <ads23a00024y@ait.edu.gh>
2024-07-01 14:53:43 +0000

    adding the menu icon to headerwrapper and removing it from the sidebar

--- src/home/components/HeaderWrapper.jsx
+++ src/home/components/HeaderWrapper.jsx
@@ -1,14 +1,26 @@
-import {BellIcon} from "@chakra-ui/icons";
-import {IconButton, Image} from "@chakra-ui/react";
+import {BellIcon, HamburgerIcon} from "@chakra-ui/icons";
+import {Drawer, DrawerContent, DrawerOverlay, IconButton, Image, useDisclosure} from "@chakra-ui/react";
 
 export const HeaderWrapper = () => {
     const defaultImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png";
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <>
-            <div className="py-8 px-10">
+            <div className="py-8 px-3 lg:px-10">
                 <div className="flex justify-between">
-                    <h1 className={'text-3xl text-[#3c3c3c] bold'}>My Dashboard</h1>
+                   <div className="flex justify-around gap-5" style={{alignItems: "center"}}>
+                       <IconButton aria-label={'see menu'} icon={<HamburgerIcon />} onClick={onOpen} display={{ base: "block", lg: "none" }}/>
+                       <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
+                           <DrawerOverlay />
+                           <DrawerContent bg="#1e1e1e">
+                               <div className="border-b border-gray-700 border-dashed p-8 ml-3">
+                                   <h1 className="font-bold text-white text-4xl ">Logo</h1>
+                               </div>
+                           </DrawerContent>
+                       </Drawer>
+                       <h1 className={'text-lg lg:text-2xl text-[#3c3c3c] bold'}>My Dashboard</h1>
+                   </div>
 
                     <div className="flex justify-around gap-5" style={{alignItems: "center"}}>
                         <IconButton variant="ghost" color="" cursor="pointer" icon={<BellIcon boxSize={6} />} aria-label={'see notifications'} />
--- src/home/components/SideBar.jsx
+++ src/home/components/SideBar.jsx
@@ -1,8 +1,6 @@
-import {Drawer, DrawerContent, DrawerOverlay, IconButton, useDisclosure} from "@chakra-ui/react";
-import {HamburgerIcon} from "@chakra-ui/icons";
+
 
 export const SideBar = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
         <>
@@ -13,16 +11,6 @@ export const SideBar = () => {
                     </div>
                 </div>
             </div>
-
-            <IconButton aria-label={'see menu'} icon={<HamburgerIcon />} onClick={onOpen} ml={3} mt={6} display={{ base: "block", lg: "none" }}/>
-            <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
-                <DrawerOverlay />
-                <DrawerContent bg="#1e1e1e">
-                    <div className="border-b border-gray-700 border-dashed p-8 ml-3">
-                        <h1 className="font-bold text-white text-4xl ">Logo</h1>
-                    </div>
-                </DrawerContent>
-            </Drawer>
         </>
     )
 }
