f2da3fbfab9bea65deca849e98c2bfee4ab11bbf
mahuena <ads23a00024y@ait.edu.gh>
2024-07-01 13:50:11 +0000

    done with header wrapper

--- src/home/components/HeaderWrapper.jsx
+++ src/home/components/HeaderWrapper.jsx
@@ -1,7 +1,21 @@
+import {BellIcon} from "@chakra-ui/icons";
+import {IconButton, Image} from "@chakra-ui/react";
+
 export const HeaderWrapper = () => {
+    const defaultImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png";
+
     return (
         <>
-            <div>HeaderWrapper</div>
+            <div className="py-8 px-10">
+                <div className="flex justify-between">
+                    <h1 className={'text-3xl text-[#3c3c3c] bold'}>My Dashboard</h1>
+
+                    <div className="flex justify-around gap-5" style={{alignItems: "center"}}>
+                        <IconButton variant="ghost" color="" cursor="pointer" icon={<BellIcon boxSize={6} />} aria-label={'see notifications'} />
+                        <Image src={defaultImage} cursor="pointer" width="50px" height="50px" objectFit="cover" borderRadius={"50%"}  />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
